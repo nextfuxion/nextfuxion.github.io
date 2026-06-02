a776a6c9a35184b5e2134470c8b7c2972f78cc34
nextfuxion <120492245+nextfuxion@users.noreply.github.com>
2026-06-02 12:00:19 -0500

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+"https://rgdciyivedmtqufuckcx.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_lpKFd8dqRtYqK9UqMmiTZg_H18PpSfO";
+
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
